547763057dccb51bc9135f9f688229dfa2c93719
Jac Wynn <jacquinwynn91@gmail.com>
2023-11-22 18:57:45 -0500

    setup fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'serif': ['PT Serif', 'serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
