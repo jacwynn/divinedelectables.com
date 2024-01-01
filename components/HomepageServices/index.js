9a1dc05173c14431e64dae838be0076ca01ef5ac
Jac Wynn <jacquinwynn91@gmail.com>
2024-01-01 18:38:17 -0500

    create homepage services section

---
+++ components/HomepageServices/index.js
@@ -0,0 +1,82 @@
+import Image from "next/image";
+
+export default function HomepageServices() {
+  return (
+    <section className="mt-[3rem] lg:mt-[-5rem] relative">
+      <Image
+        src={"/images/rosemary-stem.png"}
+        alt="rosemary stem"
+        width={215}
+        height={155}
+      />
+      <div className="container mt-12 mx-auto bg-brand-bg-grey rounded-[2.5rem] p-4 lg:p-24">
+        <div className="text-center max-w-[70rem] mx-auto mb-24">
+          <h2 className="text-[2.5rem] font-bold uppercase">
+            Take your event to the next level with our full-service catering
+            services
+          </h2>
+        </div>
+        <div className="flex flex-col justify-center space-y-16 text-center lg:space-x-16 lg:space-y-0 lg:flex-row">
+          <div className="flex flex-col lg:w-[400px]">
+            <div className="flex flex-col items-center space-y-4">
+              <Image
+                src={"/images/build-your-picnic.svg"}
+                alt="build your picnic"
+                width={184}
+                height={137}
+              />
+              <h4 className="font-bold text-[1.125rem] tracking-[0.06em]">
+                Build Your Picnic
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:w-[400px]">
+            <div className="flex flex-col items-center space-y-4">
+              <Image
+                src={"/images/tea-party.svg"}
+                alt="tea party"
+                width={184}
+                height={137}
+              />
+              <h4 className="font-bold text-[1.125rem] tracking-[0.06em]">
+                Tea Party
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:w-[400px]">
+            <div className="flex flex-col items-center space-y-4">
+              <Image
+                src={"/images/weddings.svg"}
+                alt="weddings and events"
+                width={184}
+                height={137}
+              />
+              <h4 className="font-bold text-[1.125rem] tracking-[0.06em]">
+                Weddings/Events
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Image
+        src={"/images/spaghetti.png"}
+        alt="spaghetti"
+        width={620}
+        height={500}
+        className="absolute right-0 bottom-[-10rem] w-[300px]"
+      />
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Layout from "@/components/Layout";
 
 //Components
 import HomepageHero from "@/components/HomepageHero";
+import HomepageServices from "@/components/HomepageServices";
 
 export default function Home() {
   return (
     <Layout>
       <HomepageHero />
+      <HomepageServices />
     </Layout>
   )
 }
