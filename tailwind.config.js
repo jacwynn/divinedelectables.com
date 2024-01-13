9efe807390e17b924d4e45b25e55840b257aec18
Jac Wynn <jacquinwynn91@gmail.com>
2024-01-13 12:52:17 -0500

    update tailwind config to include custom list icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'brand-red': '#D9534F',
-        'brand-black': '#333333',
-        'brand-grey': '#717171',
-        'brand-bg-grey': '#FAFAFA',
-        'brand-bg-white': '#FFFFFF'
+        "brand-red": "#D9534F",
+        "brand-black": "#333333",
+        "brand-grey": "#717171",
+        "brand-bg-grey": "#FAFAFA",
+        "brand-bg-white": "#FFFFFF",
       },
       fontFamily: {
-        'serif': ['PT Serif', 'serif'],
-        'seaweed': ['SeaweedScript', 'cursive'],
-        'berkshire': ['BerkshireSwash', 'serif']
+        serif: ["PT Serif", "serif"],
+        seaweed: ["SeaweedScript", "cursive"],
+        berkshire: ["BerkshireSwash", "serif"],
       },
-      backgroundImage: theme => ({
-        'footer-texture': 'url("/images/footer-bg.png")'
-      })
+      backgroundImage: (theme) => ({
+        "footer-texture": 'url("/images/footer-bg.png")',
+      }),
     },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".custom-list-icon": {
+          display: "flex",
+          alignItems: "center",
+          position: "relative",
+          paddingLeft: "2em",
+        },
+        ".custom-list-icon::before": {
+          content: '""',
+          display: "inline-block",
+          width: "1em",
+          height: "1em",
+          position: "absolute",
+          left: "0",
+          backgroundImage: 'url("/images/checked-circle.svg")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+        },
+      };
+      addUtilities(newUtilities, ["before"]);
+    },
+  ],
+};
