5f73806cc9b0af00c0006bf75b69b076f7772da8
Jac Wynn <jacquinwynn91@gmail.com>
2024-01-28 10:10:12 -0500

    fix text color issue on menus

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -24,7 +24,7 @@ export default function Menu({ data, ...props }) {
             <div className={`container mx-auto bg-brand-bg-grey mt-[6.25rem] rounded-[2.5rem] p-[2.5rem] lg:p-16 lg:px-0`}>
                 <div className="space-x-8 text-center">
                     {Object.keys(categories).map(category => (
-                        categories[category].length > 0 && <button key={category} onClick={() => setActiveTab(category)} className={`p-4 mt-8 text-white text-[1.75rem] uppercase font-bold ${activeTab === category ? 'bg-brand-red' : 'text-brand-red'} hover:bg-brand-red hover:text-white`}>{category}</button>
+                        categories[category].length > 0 && <button key={category} onClick={() => setActiveTab(category)} className={`p-4 mt-8 text-[1.75rem] uppercase font-bold ${activeTab === category ? 'bg-brand-red text-white' : 'bg-transparent text-brand-red'} hover:bg-brand-red hover:text-white`}>{category}</button>
                     ))}
                 </div>
                 <AdditionalItems data={additionalItems} />
