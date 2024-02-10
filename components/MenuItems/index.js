e41ea5b7d9ab58de59d93616f25f3697b33a1fa6
Jac Wynn <jacquinwynn91@gmail.com>
2024-02-10 14:59:36 -0500

    update styling and functionality for dropdown menus

--- components/MenuItems/index.js
+++ components/MenuItems/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
@@ -8,7 +9,7 @@ export default function MenuItems({ context, isMobile }) {
         { name: 'Home', href: '/' },
         { name: 'About', href: '/about' },
         { name: 'Services', href: '/services' },
-        { name: 'Menus', href: '/menus', subCats: [
+        { name: 'Menus', href: '', subCats: [
             { name: 'Build Your Picnic', href: '/menus/build-your-picnic' },
             { name: 'Tea Party', href: '/menus/tea-party' },
             { name: 'Weddings & Events', href: '/menus/weddings-and-events' },
@@ -16,6 +17,16 @@ export default function MenuItems({ context, isMobile }) {
         { name: 'Gallery', href: '/gallery' },
         { name: 'Contact', href: '/contact' },
     ];
+    
+    const [activeName, setActiveName] = useState(null);
+    const toggleDropdown = (name) => {
+        if (activeName === name) {
+            setActiveName(null);
+        } else {
+
+            setActiveName(name);
+        }
+    }
 
     return (
         isMobile ? (
@@ -29,14 +40,18 @@ export default function MenuItems({ context, isMobile }) {
         ) : (
             <div className={`flex-1 ${isFooter ? '' : 'hidden'} lg:block`}>
                 <ul className='flex flex-col uppercase lg:space-x-16 lg:flex-row'>
-                    {menuItems.map((item, idx) => (
-                        <li key={item.name} className={`group font-bold ${isFooter ? 'text-white pb-6 lg:pb-0' : 'text-brand-black pb-2'} tracking-[0.06em] border-b-2 ${(router.pathname === item.href || item.subCats && item.subCats.some(subCat => router.pathname === subCat.href)) ? 'border-brand-red' : 'border-transparent'} hover:border-brand-red`}>
-                            <Link href={item.href}>{item.name}</Link>
-                            {item.subCats && (
-                                <ul className={`absolute rounded-[2%] hidden bg-white shadow w-[18.75rem] ${!isFooter ? 'group-hover:block' : ''}`}>
+                    {menuItems.map(item => (
+                        <li key={item.name} className={`font-bold ${isFooter ? 'text-white pb-6 lg:pb-0' : 'text-brand-black pb-2'} tracking-[0.06em] border-b-2 ${activeName === item.name ? 'bg-brand-red' : ''} ${(router.pathname === item.href || item.subCats && item.subCats.some(subCat => router.pathname === subCat.href)) ? 'border-brand-red' : 'border-transparent'} hover:border-brand-red`} {...(item.subCats ? { onClick: () => toggleDropdown(item.name) } : {})}>
+                            {item.subCats?.length > 0 ? (
+                                <div className={`${activeName === item.name ? 'bg-[#AF3F3C] text-white' : ''} inline cursor-pointer p-4`}>{item.name}</div>
+                            ) : (
+                                <Link href={item.href || ''} className={`${activeName === item.name ? 'bg-brand-red' : ''} p-4`}>{item.name}</Link>
+                            )}
+                            {item.subCats && activeName === item.name && (
+                                <ul className={`bg-[#AF3F3C] absolute block top-[75px] w-[20rem]`}>
                                     {item.subCats.map(subItem => (
-                                        <li key={subItem.name} className="border-b-2 font-normal text-brand-black tracking-[0.03em]">
-                                            <Link className="block p-[1.5rem] pr-[3rem] hover:bg-gray-100 hover:text-red-500 hover:font-bold transition duration-300 ease-in-out" href={subItem.href}>{subItem.name}</Link>
+                                        <li key={subItem.name} className="font-normal p-[0.75rem] text-brand-black tracking-[0.03em]">
+                                            <Link className={`${activeName === item.name ? 'text-white' : ''} block p-[0.75rem] hover:bg-brand-red hover:font-bold transition duration-300 ease-in-out`} href={subItem.href}>{subItem.name}</Link>
                                         </li>
                                     ))}
                                 </ul>
